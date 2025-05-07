263882c381ed39c3d525c8bfda194391703f2e55
swetapadmaswain <swetapadma2693@gmail.com>
2025-05-07 17:06:49 +0530

    upt

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.97.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket = "my-tf-test-bucket-bdgdko-nsjs"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
