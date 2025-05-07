06e899887cf6a7b692ac6e7edf0132dc5ef12a74
swetapadmaswain <swetapadma2693@gmail.com>
2025-05-07 18:00:53 +0530

    tu

---
+++ terraform/variable.tf
@@ -0,0 +1,34 @@
+variable "x" {
+  default = 100
+}
+
+output "x" {
+  value = var.x
+}
+
+variable "list" {
+  default = ["abc","cba", 7]
+}
+
+variable "map" {
+  default = {
+    x = 10
+    y = 20
+  }
+}
+
+output "y" {
+  value = var.list
+}
+
+output "y_1" {
+  value = var.list[1]
+}
+
+output "map" {
+  value = var.map
+}
+
+output "m_1" {
+  value = var.map[x]
+}
