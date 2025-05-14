bb9af7f0be03d52cf07794f15f7353a1f0cf2980
swetapadmaswain <swetapadma2693@gmail.com>
2025-05-14 13:04:36 +0530

    uo

---
+++ docker/java/main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+        Date currentdate = new Date();
+        System.out.println("hello the date today is" + currentdate);
+        
+    }
+}
